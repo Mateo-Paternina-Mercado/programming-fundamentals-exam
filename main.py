0b003e1962c2b3bf1d63ab20f3645f2fe9952000
Mateo-Paternina-Mercado <185979424+Mateo-Paternina-Mercado@users.noreply.github.com>
2024-11-05 13:05:51 -0500

    Solution exercise 4

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-voltaje1 = float(input("Ingresa el primer voltaje: "))
-voltaje2 = float(input("Ingresa el segundo voltaje: "))
-voltaje3 = float(input("Ingresa el tercer voltaje: "))
+metros = float(input("Ingrese la distancia en metros: "))
 
-promedio = (voltaje1 + voltaje2 + voltaje3) / 3
+kilometros = metros / 1000
 
-if promedio < 115:
-    print("VOLTAJE CORRECTO")
-elif 115 <= promedio < 220:
-    print("ALTO VOLTAJE")
-else:
-    print("PELIGRO")
+print(f"La distancia de {metros} metros equivale a {kilometros} kilómetros.")
